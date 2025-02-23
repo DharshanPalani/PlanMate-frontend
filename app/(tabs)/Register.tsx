3ba75eb8343d1e85e28ccfaa6bb1425f117275ed
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-02-23 19:09:00 +0530

    Linked API for login/registration

--- app/(tabs)/Register.tsx
+++ app/(tabs)/Register.tsx
@@ -1,17 +1,40 @@
 import { View, Text, TextInput, Button } from 'react-native';
-import React from 'react'
-
+import React, { useState } from 'react';
+import { registerUser } from '../api/api';
 import formStyles from '@/css/FormStyle';
 
 const RegisterScreen = () => {
+    const [username, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegistration = async () => {
+        try {
+            const data = await registerUser(username, password);
+            alert(data.message);
+        } catch (e: any) {
+            alert(e.response?.data?.error || "Registration failed");
+        }
+    };
+
     return (
         <View style={formStyles.container}>
             <Text style={formStyles.title}>Register</Text>
-            <TextInput style={formStyles.input} placeholder="Email" />
-            <TextInput style={formStyles.input} placeholder="Password" secureTextEntry />
-            <Button title="Register" />
+            <TextInput
+                style={formStyles.input}
+                placeholder="Email"
+                onChangeText={setUserName}
+                value={username}
+            />
+            <TextInput
+                style={formStyles.input}
+                placeholder="Password"
+                secureTextEntry
+                onChangeText={setPassword}
+                value={password}
+            />
+            <Button title="Register" onPress={handleRegistration} />
         </View>
-    )
-}
+    );
+};
 
-export default RegisterScreen
+export default RegisterScreen;
