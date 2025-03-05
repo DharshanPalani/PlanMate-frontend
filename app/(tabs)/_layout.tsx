575a1722c3fc4430c162f1e4a6469225de8f5535
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-03-05 18:04:24 +0530

    Styled and tweaks in register page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,7 +10,7 @@ export default function Layout() {
       <Stack.Screen name="index" options={{ title: "Home" }} />
       <Stack.Screen name="Login" options={{ title: "Welcome back!" }} />
       <Stack.Screen name="Register" options={{ title: "Get started!" }} />
-      <Stack.Screen name="dashboard" options={{ title: "Dashboard" }} />
+      <Stack.Screen name="Dashboard" options={{ title: "Dashboard" }} />
     </Stack>
   );
 }
